d345016f8c86447c6ddadadac7f410bd50dfdbd0
thaisduarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-07-08 10:06:36 -0300

    add - ex_04

---
+++ exercícios/ex_04.php
@@ -0,0 +1,16 @@
+<?php
+function gerarSenha($tamanho) {
+    $caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*";
+
+    $senha = "";
+
+    for ($i = 0; $i < $tamanho; $i++) {
+        $indice = rand(0, strlen($caracteres) - 1);
+        $senha .= $caracteres[$indice];
+    }
+
+    return $senha;
+}
+
+echo gerarSenha(10);
+?>
