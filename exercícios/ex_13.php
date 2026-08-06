8082f01e512450098ea2e1c487c9b4fbff602da6
ThaisDuarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-08-06 17:36:55 -0300

    add - ex_13

--- exercícios/ex_13.php
+++ exercícios/ex_13.php
@@ -1,4 +1,37 @@
 <?php
 
+function criptografarMensagem($texto){
+
+    $resultado = "";
+
+    for($i = 0; $i < strlen($texto); $i++){
+
+        $resultado .= chr(ord($texto[$i]) + 3);
+
+    }
+
+    return $resultado;
+}
+
+function descriptografarMensagem($texto){
+
+    $resultado = "";
+
+    for($i = 0; $i < strlen($texto); $i++){
+
+        $resultado .= chr(ord($texto[$i]) - 3);
+
+    }
+
+    return $resultado;
+}
+
+$mensagem = "OLAAA";
+
+$cripto = criptografarMensagem($mensagem);
+
+echo "Mensagem original: " . $mensagem;
+echo "<br>Criptografada: " . $cripto;
+echo "<br>Descriptografada: " . descriptografarMensagem($cripto);
 
 ?>
