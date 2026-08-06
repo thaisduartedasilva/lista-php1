58349a0566565909d94f868435f8bcaa4d1d97f2
ThaisDuarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-08-06 16:10:58 -0300

    add - ex_10

--- exercícios/ex_10.php
+++ exercícios/ex_10.php
@@ -1,11 +1,30 @@
 <?php
 
-function calcularMedia($nota1, $nota2, $nota3){
-    if ($nota1 > $nota2 && $nota1 > $nota3){
+function calcularMedia($notas){
 
+    $maior = max($notas);
+    $menor = min($notas);
+    $soma = array_sum ($notas);
+    $media = $soma / count($notas);
+
+    if ($media >= 7){
+        $situacao_f = "Aprovado!";
+
+    }elseif ($media <= 6 && $media >= 4){
+        $situacao_f = "Recuperação!";
+
+    }else{
+        $situacao_f = "Reprovado!";
     }
 
-    
+    return "Maior nota: " .$maior.
+           "<br>Menor nota: " .$menor.
+           "<br>Média: " .$media.
+           "<br>Situação: " .$situacao_f;
 }
+
+$notas = [8, 6, 9, 7];
+
+echo calcularMedia($notas);
 
 ?>
